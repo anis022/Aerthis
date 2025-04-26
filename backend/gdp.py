bc085b2750658c3bf5374042612a5a8c2f4ddd94
vlrr <comptedecode@gmail.com>
2025-04-26 17:35:43 -0400

    updated bckend bcz im fullstack and teamates are sleeping

--- backend/gdp.py
+++ backend/gdp.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def get_gdp_data():
     data = pd.read_csv('data/gdp.csv', usecols=['Country Name','Country Code', 'Year', 'Value'])
-    return data
+    return data.to_json()
--- backend/spending.py
+++ backend/spending.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-data = pd.read_excel('data/lol.xlsx', sheet_name='EM-DAT Data', usecols=['Country', 'target'])
-print(data.to_json)
+def get_spending_data():
+    data = pd.read_excel('data/lol.xlsx', sheet_name='EM-DAT Data', usecols=['Country', 'target'])
+    return data.to_json()
