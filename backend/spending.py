1bf7560259c76ad6b5ca252f80f7a74aeea1a093
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-26 17:47:10 -0400

    Refactor spending.py to sum target spending by country and update requirements.txt to include openpyxl

--- backend/spending.py
+++ backend/spending.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-def get_spending_data():
-    data = pd.read_excel('data/lol.xlsx', sheet_name='EM-DAT Data', usecols=['Country', 'target'])
-    return data.to_json()
+data = pd.read_excel('data/lol.xlsx', sheet_name='EM-DAT Data',
+                     usecols=['Country', 'target'])
+
+# Group by 'Country' and sum 'target'
+summed_spending = data.groupby('Country')['target'].sum().to_dict()
+
+print(summed_spending)
