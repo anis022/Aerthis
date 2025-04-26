d902b2d11d3d5f87727f1dc18a3a6c2ae1652e42
alexis andrew <83471846+alexis-martel@users.noreply.github.com>
2025-04-26 16:53:23 -0400

    Add data processing scripts

---
+++ api_demo.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# generate a list of geo coords
+for lng in range(-180, 180):
+    for lat in range(-90, 90):
+        print(f"lat: {lat}, lng: {lng}")
+        req = requests.get(f"https://api.waqi.info/feed/geo:{lat};{lng}/?token={"75001dc18d56abf731eb69047a75664d4267c47d"}")
+        req_json = json.loads(req.text)
+        db_dict = {
+            "loc": req_json["data"]["city"]["name"],
+            "aqi": req_json["data"]["aqi"],
+            "lat": req_json["data"]["city"]["geo"][0],
+            "lng": req_json["data"]["city"]["geo"][1],
+            "tim": req_json["data"]["time"]["s"],
+            "src": req_json["data"]["attributions"][0]["url"],
+            "dom": req_json["data"]["dominentpol"],
+            "iaq": req_json["data"]["iaqi"],
+        }
+        print(json.dumps(db_dict,separators=(',', ':')))
+        with open("aqi_data.txt", "a") as f:
+            f.write(json.dumps(db_dict, separators=(',', ':')))
+            f.write("\n")
