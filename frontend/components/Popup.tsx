a7f3adf8d606bfbee92bedbd118399cb6e23dba0
alexis andrew <83471846+alexis-martel@users.noreply.github.com>
2025-04-27 05:36:43 -0400

    Update Popup.tsx

--- frontend/components/Popup.tsx
+++ frontend/components/Popup.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import styles from './Popup.module.css';
+import React from "react";
+import styles from "./Popup.module.css";
 
 interface PopupData {
   "Air Quality Index": number;
   "Biggest Air Polluant": string;
-  "Country": string;
+  Country: string;
   "Disaster Spending": number;
   "Dominant Pollutants": string;
-  "GDP": number;
+  GDP: number;
   "Percentage of GDP used on disaster spending": number;
   "Plastic Pollution": number;
   "Recap of Pollution": string;
@@ -15,33 +15,51 @@ interface PopupData {
   "Temperature Difference": number;
 }
 
-const Popup = ({ jsonData }: { jsonData: any}) => {
+const Popup = ({ jsonData }: { jsonData: any }) => {
   const country = jsonData?.["Country"] ?? "No country selected";
   const GDP = jsonData?.["GDP"]?.toLocaleString() ?? "-";
-  const disasterSpending = jsonData?.["Disaster Spending"]?.toLocaleString() ?? "-";
-  const percentageGDPusedOnDisasterSpending = jsonData?.["Percentage of GDP used on disaster spending"]?.toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 }) ?? "-";
+  const disasterSpending =
+    jsonData?.["Disaster Spending"]?.toLocaleString() ?? "-";
+  const percentageGDPusedOnDisasterSpending =
+    jsonData?.["Percentage of GDP used on disaster spending"]?.toLocaleString(
+      undefined,
+      { style: "percent", minimumFractionDigits: 2 },
+    ) ?? "-";
   const dominantPollutants = jsonData?.["Dominant Pollutants"] ?? "-";
-  const plasticPollution = jsonData?.["Plastic Pollution"]?.toLocaleString() ?? "-";
+  const plasticPollution =
+    jsonData?.["Plastic Pollution"]?.toLocaleString() ?? "-";
   const recapOfPollution = jsonData?.["Recap of Pollution"] ?? "-";
   const solution = jsonData?.["Solution Suggestion"] ?? "-";
   const aqi = jsonData?.["Air Quality Index"] ?? "-";
   const biggestPollutant = jsonData?.["Biggest Air Polluant"] ?? "-";
-  const temperatureDiff = jsonData?.["Temperature Difference"]?.toFixed(2) ?? "-";
+  const temperatureDiff =
+    jsonData?.["Temperature Difference"]?.toFixed(2) ?? "-";
 
   return (
-    <div className={`${styles.popupContainer} flex items-center justify-end`} >
+    <div className={`${styles.popupContainer}`}>
       <div className={styles.popup}>
-        <h2>Popup Title</h2>
-        <p>This is a message inside the popup.</p>
-        <p>Country: {country}</p>
-        <p>GDP: {GDP}</p>
-        <p>Percentage of GDP used on disaster spending: {percentageGDPusedOnDisasterSpending}</p>
-        <p>Solution: {solution}</p>
-
-        <span>X</span>
+        <h1>Aerthis</h1>
+        <hr></hr>
+        <h2>About this Country: </h2>
+        <p className={styles.countryName}>{country}</p>
+        <ul>
+          <li>
+            <span className={styles.statTitle}>GDP:</span> {GDP}
+          </li>
+          <li>
+            <span className={styles.statTitle}>
+              % of GDP Spent on Disaster Relief:
+            </span>{" "}
+            {percentageGDPusedOnDisasterSpending}
+          </li>
+        </ul>
+        <hr className={styles.thinHR}></hr>
+        <p>
+          <span className={styles.statTitle}>Future Prospects:</span> {solution}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popup
+export default Popup;
