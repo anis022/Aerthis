597f7a326b898f559f6aea38acba1bdbfa5247ed
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-27 10:54:44 -0400

    Change default port to 10000 for application startup

--- backend/app.py
+++ backend/app.py
@@ -141,5 +141,5 @@ def get_coordinates():
 #     return get_data(coords)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
