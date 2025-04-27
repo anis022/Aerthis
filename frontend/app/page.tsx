c8607aee844cd42ee2ef5dbf706dad7c229e58b7
vlrr <comptedecode@gmail.com>
2025-04-26 20:10:37 -0400

    removed comments

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,16 +8,6 @@ const DynamicEarthGlobe = dynamic(() => import('@/components/EarthGlobe'), {
   loading: () => <EarthLoading />, // Optional loading indicator
 });
 
-// const DynamicEarthGlobe = dynamic(() => new Promise((resolve) => {
-//     // Force a 5 second delay
-//     setTimeout(() => {
-//       resolve(import('@/components/EarthGlobe'))
-//     }, 5000) // 5000ms = 5 seconds
-//   }), {
-//   ssr: false,
-//   loading: () => <EarthLoading />
-// });
-
 export default function Home() {
   return (
     <div>
