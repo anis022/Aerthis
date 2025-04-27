0c48ac82a6b9e7eb9ca991ab611f67c708d12b39
vlrr <comptedecode@gmail.com>
2025-04-26 23:39:38 -0400

    last bacup commit

--- frontend/components/EarthGlobe.tsx
+++ frontend/components/EarthGlobe.tsx
@@ -89,6 +89,7 @@ const EarthGlobe = ({ heatmapData }: any) => {
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg" // Basic globe texture
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png" // Optional: adds terrain texture
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png" // Optional: star background
+        // globeImageUrl="world.200412.3x21600x10800.jpg"
         
         atmosphereColor="lightskyblue"
         atmosphereAltitude={0.25}
