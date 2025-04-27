7f1d4041fd40bb9d792ab263ef792259a0aac9c3
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-27 01:57:39 -0400

    Refactor geo data handling by consolidating logic into get_data function and reordering route definitions for clarity

--- backend/app.py
+++ backend/app.py
@@ -6,36 +6,15 @@ import reverse_geocode
 import pandas as pd
 import json
 from closest_coords import find_closest_location_and_give_aqi
+from api import generate_coordinates_response
 # Load environment variables from .env file
 load_dotenv()
 import db
 # Create a Flask application instance
 app = Flask(__name__)
 # Add CORS headers to all responses
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = os.getenv("FRONTEND_URL")
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    return response
-
-# route that will get longitude and lattitude as args
-@app.route('/get-geo-data', methods=['POST'])
-def get_geo_data():
-    if request.method == 'OPTIONS':
-        return '', 200
-
-    print('Request method:', request.method)
-    print('Request headers:', dict(request.headers))
-
-    data = request.get_json()
-    print('Received data:', data)
-    lat = float(data.get('lat'))
-    lng = float(data.get('lng'))
-    print(f"Received lat: {lat}, lng: {lng}")
-    # info for the popup on the map
-    coords= (lat, lng)
+def get_data(coords):
+    lat, lng = coords
     country_code_2_letter = reverse_geocode.search([coords])[0]['country_code']
     code_data = pd.read_csv('data/isocode.csv')
     country_code_3_letter = code_data.loc[code_data['A2'] == country_code_2_letter, 'A3'].values[0]
@@ -71,6 +50,32 @@ def get_geo_data():
         'temp_diff': tmp,
         'aqi': data_aqi,
     })
+    
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = os.getenv("FRONTEND_URL")
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+# route that will get longitude and lattitude as args
+@app.route('/get-geo-data', methods=['POST'])
+def get_geo_data():
+    if request.method == 'OPTIONS':
+        return '', 200
+
+    print('Request method:', request.method)
+    print('Request headers:', dict(request.headers))
+
+    data = request.get_json()
+    print('Received data:', data)
+    lat = float(data.get('lat'))
+    lng = float(data.get('lng'))
+    print(f"Received lat: {lat}, lng: {lng}")
+    # info for the popup on the map
+    coords= (lat, lng)
+    return get_data(coords=coords)
 
 @app.route('/search', methods=['POST'])
 def get_search():
@@ -80,9 +85,11 @@ def get_search():
     search_query = data.get('search')  # 'search' should match what you sent from React
     # Do something useful here if you want (search a DB, filter data, etc.)
 
-    return jsonify({
-        "search": search_query
-    }), 200
+    coords = generate_coordinates_response(search_query)
+    coords = json.loads(coords)
+    coords = (float(coords['lat']), float(coords['lng']))
+    print(coords)
+    return get_data(coords)
 
 if __name__ == '__main__':
     app.run(debug=True)
