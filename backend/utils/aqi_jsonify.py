252e4f742e34ac6df42c634a65c605cabde7df1c
alexis andrew <83471846+alexis-martel@users.noreply.github.com>
2025-04-26 20:56:01 -0400

    Add duplicate AQI entry removal

--- backend/utils/aqi_jsonify.py
+++ backend/utils/aqi_jsonify.py
@@ -4,6 +4,8 @@ dict = {"aqi": [],}
 
 with open('../data-processed/aqi.json', 'w') as f:
     with open('../../aqi_DATA.txt', 'r') as lines:
-        for line in lines:
+        set_lines = set(lines)
+        list_lines = list(set_lines)
+        for line in list_lines:
             dict['aqi'].append(json.loads(line.strip()))
     json.dump(dict, f, indent=None, separators=(',', ':'))
