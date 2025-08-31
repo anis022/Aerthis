d1f3539116bc7ec67e6914b5f9ad166dd2100644
vlrr <comptedecode@gmail.com>
2025-08-31 17:49:15 -0400

    updating typo

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: "aerthis-backend.onrender.com",
+    NEXT_PUBLIC_API_URL: "https://aerthis-backend.onrender.com",
   },
   eslint: {
     ignoreDuringBuilds: true,
