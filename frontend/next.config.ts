2ddc0e70f2957bdd6bafe7bf5972f343bf7c3ce5
vlrr <comptedecode@gmail.com>
2025-10-29 20:04:48 -0400

    Changed backend url for testing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: "https://aerthis-backend-wq42.onrender.com",
-    // NEXT_PUBLIC_API_URL: "http://localhost:8000",
+    // NEXT_PUBLIC_API_URL: "https://aerthis-backend-wq42.onrender.com",
+    NEXT_PUBLIC_API_URL: "http://localhost:8000",
   },
   eslint: {
     ignoreDuringBuilds: true,
